11ccaa7f5f0097e69cf725b39d11dd4c913caba7
Gnyaneshwari22 <gnyaneshwariadki2@gmail.com>
2023-11-21 04:39:27 +0530

    Input Field Validation added

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let URL="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 
 function QRCodeGeneration(){
    let Inputvalue=inputFeild.value;
+   if(Inputvalue == ''){
+     alert("Please enter Text or URL in the input Feild");
+   }
     qrimage.src=URL + Inputvalue ;
     console.log("Code generated successfully");
 
